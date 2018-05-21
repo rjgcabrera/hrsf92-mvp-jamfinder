8451941cd8543cb4838e22e55bb25007d4adf08a
Raymond Cabrera <rjgcengr@gmail.com>
2018-05-21 16:46:56 -0700

    Render event list with sample data

--- client/src/components/EventListEntry.jsx
+++ client/src/components/EventListEntry.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 
 let EventListEntry = (props) => {
 
+  let venue = <div></div>;
+  if (props.oneEvent.venue) {
+    // let venueAddress = `https://www.google.com/maps/place/1550+California+St+%232,+San+Francisco,+CA+94109/@37.7909273,-122.4222365`;
+    //
+    venue = <div>
+      <div><p>Venue Name: {props.oneEvent.venue.name}</p></div>
+      <div>
+        <span>Venue Address: </span>
+        <span>{props.oneEvent.venue.address_1}, </span>
+        <span>{props.oneEvent.venue.city}, {props.oneEvent.venue.state}, {props.oneEvent.venue.zip}</span>
+      </div>
+    </div>;
+  }
+
   return(
     <div className="event-list-entry">
-      <div><p>Event title</p></div>
-      <div><p>Hosted by: </p></div>
-      <div><p>Number of Attendees</p></div>
-      <div><p>Event date</p></div>
-      <div><p>Start time - End time</p></div>
-      <div><p>Venue Name</p></div>
-      <div><p>Venue Address</p></div>
+      <div><a href={props.oneEvent.link}>{props.oneEvent.name}</a></div>
+      <div><p>Hosted by: {props.oneEvent.group.name}</p></div>
+      <div><p>Number of Attendees: {props.oneEvent.yes_rsvp_count}</p></div>
+      <div><p>Event date: {props.oneEvent.local_date}</p></div>
+      <div><p>Time: {props.oneEvent.local_time}</p></div>
+      {venue}
     </div>
   )
 }
